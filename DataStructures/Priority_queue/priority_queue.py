543ffffedc6b582a07e39d985dc8f266ea90663c
jsantanilla <j.santanilla@uniandes.edu.co>
2025-05-06 10:48:27 -0500

    Estructura de datos y funciones iniciales

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,3 +1,58 @@
 from DataStructures.List import array_list as lt
+from DataStructures.Priority_queue import index_pq_entry as ipq
 
+def new_heap(is_min_pq = True): #Jsantanilla
+    heap = {'elements': lt.new_list(),
+            'size': 0,
+            'cmp_function': None}
+    lt.add_last(heap['elements'], None)
+    if is_min_pq == True:
+        heap['cmp_function'] = default_compare_lower_value
+    else: 
+        heap['cmp_function'] = default_compare_higher_value
+def default_compare_higher_value():
+    pass
+def default_compare_lower_value(nodo1, nodo2): #Jsantanilla
+    llave1 = ipq.get_key(nodo1)
+    llave2 = ipq.get_key(nodo2)
+    if llave1 <= llave2:
+        return True
+    else: 
+        return False
+def priority(my_heap, nodo1, nodo2):
+    cmp = my_heap["cmp_function"](nodo1, nodo2)
+    if cmp > 0:
+        return True
+    return False
+def insert(my_heap, key, value): #Jsantanilla
+    dato = ipq.new_pq_entry(key, value)
+    lt.add_last(my_heap['elements'], dato)
+    pos = size(my_heap)
+    my_heap = swim(my_heap, pos)
+    my_heap['size'] +=1
+    return  my_heap
 
+def swim(my_heap, pos): #Jsantanilla
+    while pos > 1:
+        padre = pos // 2
+        if pos == 1 or priority(my_heap, padre, pos) == True :
+            break
+        else:
+            lt.exchange(my_heap['elements'], padre, pos)
+            pos = padre
+    return my_heap
+            
+def size():
+    pass
+def is_empty(my_heap): #Jsantanilla
+    if my_heap['size'] == 0:
+        return True
+    return False
+def get_first_priority():
+    pass
+def remove():
+    pass
+def sink():
+    pass
+
+ 
